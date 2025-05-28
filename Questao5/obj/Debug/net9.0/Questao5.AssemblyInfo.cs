68a18bca6791561dbd2ec137cca0df14493ec09e
Hans Naumann <hans.naumann@optsolv.com.br>
2025-05-28 15:58:07 -0300

    questao 3

--- Questao5/obj/Debug/net9.0/Questao5.AssemblyInfo.cs
+++ Questao5/obj/Debug/net9.0/Questao5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de30fad44d15c18934d0253ad2d3ae755fd0163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057fa2c48016ea81d28ec2da4fa0398f28db2a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
